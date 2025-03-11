eba1823a35cae6df74d97348a9fc21929bd324f7
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-03-11 13:02:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add smooth scrolling for navigation
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(event) {
-        event.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
